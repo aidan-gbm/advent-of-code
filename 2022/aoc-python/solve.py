8698eddda4811c92a6309eca7618a080aa2339ba
aidan-gbm <aidan.mccarthy12@gmail.com>
2022-12-15 14:09:24 -0500

    Day 6

--- 2022/aoc-python/solve.py
+++ 2022/aoc-python/solve.py
@@ -1,6 +1,22 @@
 import argparse
 
 
+def day_6(path: str):
+    with open(path) as fd:
+        stream = fd.readline()
+    
+    pkt_mark = 4
+    while len(set(stream[pkt_mark-4:pkt_mark])) < 4:
+        pkt_mark += 1
+
+    msg_mark = 14
+    while len(set(stream[msg_mark-14:msg_mark])) < 14:
+        msg_mark += 1
+
+    print('[+] Part 1:', pkt_mark)
+    print('[+] Part 2:', msg_mark)
+
+
 def day_5(path: str):
     from copy import deepcopy
 
