8c164b634ba6212494a87f21c3c168bd9ec53419
aidan-gbm <aidan.mccarthy12@gmail.com>
2022-12-15 13:53:46 -0500

    Day 5

--- 2022/aoc-python/solve.py
+++ 2022/aoc-python/solve.py
@@ -1,10 +1,36 @@
 import argparse
 
-input_dir = './input/'
 
-def day_4(test: bool):
-    input_file = 'test4.txt' if test else 'input4.txt'
-    with open(input_dir + input_file) as fd:
+def day_5(path: str):
+    from copy import deepcopy
+
+    with open(path) as fd:
+        lines = fd.readlines()
+        start = lines[:lines.index('\n')]
+        steps = lines[lines.index('\n') + 1:]
+
+    stacks = [[] for _ in range(len(start[-1]) // 4)]
+    for line in start[:-1]:
+        for idx in range(len(stacks)):
+            if line[idx*4+1] != ' ':
+                stacks[idx].append(line[idx*4+1])
+
+    stacks1 = list(map(lambda s: list(reversed(s)), stacks))
+    stacks2 = deepcopy(stacks1)
+    for step in steps:
+        _, num, _, src, _, dst = step.split()
+        for _ in range(int(num)):
+            stacks1[int(dst)-1].append(stacks1[int(src)-1].pop())
+
+        stacks2[int(dst)-1].extend(stacks2[int(src)-1][-int(num):])
+        stacks2[int(src)-1] = stacks2[int(src)-1][:-int(num)]
+
+    print('[+] Part 1:', ''.join(s[-1] for s in stacks1))
+    print('[+] Part 2:', ''.join(s[-1] for s in stacks2))
+
+
+def day_4(path: str):
+    with open(path) as fd:
         pairs = map(lambda s: s.strip().split(','), fd.readlines())
 
     full = partial = 0
@@ -17,7 +43,6 @@ def day_4(test: bool):
         elif any(n in r2 for n in r1) or any(n in r1 for n in r2):
             partial += 1
 
-    
     print('[+] Part 1:', full)
     print('[+] Part 2:', partial)
 
@@ -34,5 +59,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     func = f'day_{args.day}'
+    input_file = f'test{args.day}.txt' if args.test else f'input{args.day}.txt'
     print(f'[#] Computing day {args.day}, test: {args.test}...')
-    globals().get(func, err)(args.test)
+    globals().get(func, err)('./input/' + input_file)
