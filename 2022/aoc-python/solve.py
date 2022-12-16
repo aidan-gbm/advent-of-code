43e1bda9ece0a34af4c80726e6fc2a0c97070827
aidan-gbm <aidan.mccarthy12@gmail.com>
2022-12-15 21:33:22 -0500

    Day 7

--- 2022/aoc-python/solve.py
+++ 2022/aoc-python/solve.py
@@ -1,6 +1,37 @@
 import argparse
 
 
+def day_7(path: str):
+    from collections import defaultdict
+    from itertools import accumulate
+
+    with open(path) as fd:
+        term = fd.readlines()
+
+    sizes = defaultdict(int)
+    for line in map(str.strip, term):
+        if line[0] == '$':
+            cmd, *arg = line.split()[1:]
+            if cmd == 'cd':
+                if arg[0] == '/':
+                    cwd = ['/']
+                elif arg[0] == '..':
+                    cwd.pop()
+                else:
+                    cwd.append(arg[0])
+        else:
+            [size, _] = line.split()
+            if size.isdigit():
+                for d in accumulate(cwd):
+                    sizes[d] += int(size)
+
+    needed = sizes['/'] - 40000000
+    to_del = next(filter(lambda s: s >= needed, sorted(sizes.values())))
+
+    print('[+] Part 1:', sum(filter(lambda s: s <= 100000, sizes.values())))
+    print('[+] Part 2:', to_del)
+
+
 def day_6(path: str):
     with open(path) as fd:
         stream = fd.readline()
